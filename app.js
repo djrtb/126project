089e3ba90beb8936dcd67b216495fdf23e37faf3
djrtb <dtbuna@up.edu.ph>
2017-05-29 00:32:37 +0800

    made new routing for the side-menu for modularity. minor modifications made for on hompage

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var nunjucksRender = require('gulp-nunjucks-render');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var sideMenu = require('./routes/side-menu');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/side-menu', sideMenu);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
