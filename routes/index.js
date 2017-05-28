41028d9b476fdf24c2ace2dc8dd41a31e0174a51
ahjummah <jessacabigas@gmail.com>
2017-05-28 21:03:35 +0800

    Added route for trafficking.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/vawc', function(req, res) {
+  res.render('vawc', { title: 'VAWC' });
+});
+
+router.get('/trafficking', function(req, res) {
+  res.render('trafficking', { title: 'Trafficking' });
+});
+
+
+
 module.exports = router;
