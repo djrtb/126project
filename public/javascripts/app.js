02f4ab0384deb6921b00039f3c290dd31f258e54
djrtb <dtbuna@up.edu.ph>
2017-05-28 23:54:37 +0800

    added template for pages

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,4 @@
+//For homepage
 $(document).ready(function(){
 	$(window).scroll(function(){
      var top=$(window).scrollTop()
@@ -18,7 +19,7 @@ $(document).ready(function(){
      }
  	})
 
-
+//For slideshow
 
 });
 
@@ -50,3 +51,52 @@ $(function () {
     setTimeout(nextBackground, 5000);
     events.css('background', backgrounds[0]);
 });
+
+//For individual pages
+
+(function (window, document) {
+
+    var layout   = document.getElementById('layout'),
+        menu     = document.getElementById('menu'),
+        menuLink = document.getElementById('menuLink'),
+        content  = document.getElementById('main');
+
+    function toggleClass(element, className) {
+        var classes = element.className.split(/\s+/),
+            length = classes.length,
+            i = 0;
+
+        for(; i < length; i++) {
+          if (classes[i] === className) {
+            classes.splice(i, 1);
+            break;
+          }
+        }
+        // The className is not found
+        if (length === classes.length) {
+            classes.push(className);
+        }
+
+        element.className = classes.join(' ');
+    }
+
+    function toggleAll(e) {
+        var active = 'active';
+
+        e.preventDefault();
+        toggleClass(layout, active);
+        toggleClass(menu, active);
+        toggleClass(menuLink, active);
+    }
+
+    menuLink.onclick = function (e) {
+        toggleAll(e);
+    };
+
+    content.onclick = function(e) {
+        if (menu.className.indexOf('active') !== -1) {
+            toggleAll(e);
+        }
+    };
+
+}(this, this.document));
