2e92e94bb9852cb1696a59c5853331e7b92f786e
aprildaebation <adaecb@gmail.com>
2017-05-28 23:44:58 +0800

    added route/view for trafficking

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/trafficking', function(req, res, next) {
+  res.render('trafficking', { title: 'Sex Trafficking' });
+});
+
 module.exports = router;
