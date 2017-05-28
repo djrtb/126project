cf955777ef9214e4e898e1dcd1589b54605834c6
its-czarina <czarinacahutay@gmail.com>
2017-05-28 23:13:46 +0800

    added html code for anti-pornography page

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var nunjucksRender = require('gulp-nunjucks-render');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var pornography = require('./routes/pornography');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/pornography', pornography);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
